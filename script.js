46f56f620b2520f130847dba49ad79b412713c26
nlopezAQ <nlopez@accessq.com.mx>
2026-06-01 16:41:26 -0600

    Slider ascultación

--- script.js
+++ script.js
@@ -99,52 +99,55 @@ if (headerPlaceholder) {
 
 // --- FUNCIÓN SLIDER HERO
 
-let list = document.querySelector(".slider .list");
-let items = document.querySelectorAll(".slider .list .item");
-let dots = document.querySelectorAll(".slider .dots li");
-let prev = document.getElementById("prev");
-let next = document.getElementById("next");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
 
-let active = 0;
-let lengthItems = items.length - 1;
+if (next && prev) {
+  let list = document.querySelector(".slider .list");
+  let items = document.querySelectorAll(".slider .list .item");
+  let dots = document.querySelectorAll(".slider .dots li");
 
-next.onclick = function () {
-  if (active + 1 > lengthItems) {
-    active = 0;
-  } else {
-    active = active + 1;
-  }
-  reloadSlider();
-};
+  let active = 0;
+  let lengthItems = items.length - 1;
 
-prev.onclick = function () {
-  if (active - 1 < 0) {
-    active = lengthItems;
-  } else {
-    active = active - 1;
-  }
-  reloadSlider();
-};
-
-let refreshSlider = setInterval(() => {next.click()}, 3000);
-
-function reloadSlider() {
-  let checkLeft = items[active].offsetLeft;
-  list.style.left = -checkLeft + "px";
-
-  let lastActiveDot = document.querySelector(".slider .dots li.active");
-  lastActiveDot.classList.remove("active");
-  dots[active].classList.add("active");
-  clearInterval(refreshSlider);
-  refreshSlider = setInterval(() => {next.click()}, 3000);
-}
-
-dots.forEach((li, key) => {
-  li.addEventListener('click', function (){
-    active = key;
+  next.onclick = function () {
+    if (active + 1 > lengthItems) {
+      active = 0;
+    } else {
+      active = active + 1;
+    }
     reloadSlider();
-  })
-})
+  };
+
+  prev.onclick = function () {
+    if (active - 1 < 0) {
+      active = lengthItems;
+    } else {
+      active = active - 1;
+    }
+    reloadSlider();
+  };
+
+  let refreshSlider = setInterval(() => {next.click()}, 3000);
+
+  function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + "px";
+
+    let lastActiveDot = document.querySelector(".slider .dots li.active");
+    lastActiveDot.classList.remove("active");
+    dots[active].classList.add("active");
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(() => {next.click()}, 3000);
+  }
+
+  dots.forEach((li, key) => {
+    li.addEventListener('click', function (){
+      active = key;
+      reloadSlider();
+    });
+  });
+}
 
      // --- SECCIÓN 2: LÓGICA DE LAS PESTAÑAS (sin cambios) ---
     const tabSection = document.querySelector('.page-tab-section');
